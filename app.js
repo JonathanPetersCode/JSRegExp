91656bb8d374598af0cd1b35e489ea7c804a638f
Jonathan Peters <42306733+JonathanPetersCode@users.noreply.github.com>
2018-11-23 18:17:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -39,3 +39,69 @@ re = /hello/i; // i = case insensative
 const str = "Hello There";
 const newStr= str.replace(re, "Hi");
 console.log(newStr);
+
+let re;
+//Literal characters
+re = /hello/;
+re = /hello/i;
+
+//Metacharcter Symbols
+re = /^h/i  // Must start with 
+re = / world$/i  // Must start with 
+re = /^hello$/i // Must begin an end with
+re = /h.llo/i // Matches any ONE character
+re = /h*llo/i // Matches any character 0 or more times
+
+re = /gre?a?y/i; // Optional Character
+re = /gre?a?y\?/i; // Escape Character
+
+//Brackets [] - Character Sets 
+re = /gr[ae]y/i; // Must be an "a" or "e" 
+re = /[GF]ray/i; // Must be an "G" or "F" 
+re = /[^GF]ray/i; // Match anything except of a "G" or "F"
+re = /[A-Z]ray/; // Match anything any uppercase letter 
+re = /[a-z]ray/; // Match anything any lowercase letter 
+re = /[A-Za-z]ray/; // Match anything any letter, of any case 
+re = /[0-9][0-9]ray/; //Match any digit
+
+//Braces {} - Quantifiers
+
+re = /Hel{2}o/i; // Must occur exactly {m} amount of times
+re = /Hel{2,4}o/i; // Must occur exactly between {m} amount of times
+re = /Hel{2,}o/i; // Must occur at least {m} time
+
+//Parentheses () - Grouping 
+re = /^([0-9]x){3}$/ 
+
+
+//Shorthand Character Classes
+re = /\w/; //Word character - alphanumeric or -
+re = /\w+/; //+ = One or more word characters
+re = /\W/; //+ = One or more word characters
+re = /\d/; //Match any digit
+re = /\d+/; //Match any digit 0 or more times
+re = /\D/; //Match any non-digits
+re = /\s/; //Match white-space char
+re = /\S/; //Match non-whitespace char
+re = /Hell\b/i; //Word boundary
+
+//Assertions 
+re = /x(?=y)/; //Match x only is followed by y
+re = /x(?!y)/; //Match x only NOT followed by y
+
+// String to match
+const str= "dljekxsdsd";
+
+//Log Results
+const result = re.exec(str);
+console.log(result);
+
+function reTest(re, str){
+  if(re.test(str)){
+    console.log(`${str} matches ${re.source}`);
+  } else {
+    console.log(`${str} does NOT match ${re.source}`)
+  }
+}
+
+reTest(re, str);
